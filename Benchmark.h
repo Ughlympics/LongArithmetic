60864c595b11d31a932bb6ea79b8ed6bc8f8fe0b
Kirill Radkevich <Ughlympics>
2024-10-18 17:03:36 +0300

    Benchmark class include

---
+++ Benchmark.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+#include <chrono>
+
+class Benchmark
+{
+public:
+    Benchmark(const std::string& operationName = "Operation")
+        : start(std::chrono::high_resolution_clock::now()), name(operationName) {}
+
+    ~Benchmark() {
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed = end - start;
+        std::cout << name << " took " << elapsed.count() << " seconds.\n";
+    }
+
+    void stop() {
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsed = end - start;
+        std::cout << name << " took " << elapsed.count() << " seconds.\n";
+    }
+
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> start;
+    std::string name;
+};
+
